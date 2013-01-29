aa40090b24d5f78ffa5aecc9c9b5778f315ad4f3
Alexey Vishnevski <vishnevski.a@gmail.com>
2013-01-29 18:13:08 +0300

    Logging reconfigured.

--- axes/decorators.py
+++ axes/decorators.py
@@ -71,8 +71,8 @@ def ip_in_blacklist(ip):
 
 log = logging.getLogger(gs('LOGGER'))
 if gs('VERBOSE'):
-    log.info('AXES: BEGIN LOG')
-    log.info('Using django-axes ' + axes.get_version())
+    log.debug('AXES: BEGIN LOG')
+    log.debug('Using django-axes ' + axes.get_version())
 
 def is_user_lockable(request):
     """ Check if the user has a profile with nolockout
@@ -148,11 +148,11 @@ def watch_login(func):
     def decorated_login(request, *args, **kwargs):
         # share some useful information
         if func.__name__ != 'decorated_login' and gs('VERBOSE'):
-            log.info('AXES: Calling decorated function: %s' % func.__name__)
+            log.debug('AXES: Calling decorated function: %s' % func.__name__)
             if args:
-                log.info('args: %s' % args)
+                log.debug('args: %s' % args)
             if kwargs:
-                log.info('kwargs: %s' % kwargs)
+                log.debug('kwargs: %s' % kwargs)
 
         # TODO: create a class to hold the attempts records and perform checks
         # with its methods? or just store attempts=get_user_attempts here and
